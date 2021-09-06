318c6d92a2663a60dc06d6f3b0008ccc88857657
Ghani Hakim <ghani.ouighour@gmail.com>
2021-09-06 11:06:20 +0200

    add users controller

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -12,6 +12,38 @@ export class UsersService {
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
 
+    //we save a user after created it, this way all the listeners and subscribers
+    //will be applied to User Entity.Alternatively we can pass a plain object to
+    // save method to save a user, but in this way all the methods tied to User
+    // Entity will be ignored
     return this.repo.save(user);
+  }
+
+  async findOne(id: number) {
+    const user = await this.repo.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
+  }
+
+  findAll(email: string) {
+    return this.repo.find({ email });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+
+    Object.assign(user, attrs);
+
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    return this.repo.remove(user);
+    // Alternatively we can use delete method but this dosnt trigger Entity listeners
   }
 }
